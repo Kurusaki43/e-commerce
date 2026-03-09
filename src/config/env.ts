19d9d9f61054bf5b05773f50552737697a55f0e8
amine <m.boularaoui43@gmail.com>
2026-03-09 16:29:52 +0100

    chore: add prettier format command to scripts

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,7 +6,7 @@ dotenv.config()
 const env = cleanEnv(process.env, {
   NODE_ENV: str({ choices: ['development', 'test', 'production', 'staging'] }),
   PORT: port({ default: 5000 }),
-  MONGO_URI: str()
+  MONGO_URI: str(),
 })
 
 export default env
