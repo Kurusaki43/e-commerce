b9fe6e1a54e6a66d2f046b8bcdb3a96717b0305a
amine <m.boularaoui43@gmail.com>
2026-03-09 18:07:22 +0100

    chore: add pino log level to environment variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,6 +7,7 @@ const env = cleanEnv(process.env, {
   NODE_ENV: str({ choices: ['development', 'test', 'production', 'staging'] }),
   PORT: port({ default: 5000 }),
   MONGO_URI: str(),
+  LOG_LEVEL: str({ choices: ['error', 'warn', 'info', 'http', 'verbose', 'debug', 'silly'] }),
 })
 
 export default env
