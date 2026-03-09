cbfc47c2d09f2e4ae0c8fceb14e436c940e14d26
amine <m.boularaoui43@gmail.com>
2026-03-09 18:12:36 +0100

    chore: add pino logger configuration

---
+++ src/config/logger.ts
@@ -0,0 +1,18 @@
+import pino from 'pino'
+
+import env from './env'
+const logger = pino({
+  level: env.LOG_LEVEL || 'info',
+  transport: !env.isProd
+    ? {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'HH:MM:ss',
+          ignore: 'pid,hostname',
+        },
+      }
+    : undefined,
+})
+
+export default logger
