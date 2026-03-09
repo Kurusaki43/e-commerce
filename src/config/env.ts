05da84824083602b38ed4d5e4d02cdea450373c9
amine <m.boularaoui43@gmail.com>
2026-03-09 15:48:35 +0100

    feat: valid mongoURI using envalid

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,8 @@ import { cleanEnv, str, port } from 'envalid'
 
 const env = cleanEnv(process.env, {
   NODE_ENV: str({ choices: ['development', 'test', 'production', 'staging'] }),
-  PORT: port({ default: 5000 })
+  PORT: port({ default: 5000 }),
+  MONGO_URI: str()
 })
 
 export default env
