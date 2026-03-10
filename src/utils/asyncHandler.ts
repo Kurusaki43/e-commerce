043064ad81573b52b76ecd09171741aa5f5ed4a9
amine <m.boularaoui43@gmail.com>
2026-03-10 13:55:39 +0100

    feat: implement async handler to catch error in async request

---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,8 @@
+import type { Request, Response, NextFunction } from 'express'
+
+type AsyncFunction = (req: Request, res: Response, next: NextFunction) => Promise<unknown>
+
+export const catchAsync =
+  (fn: AsyncFunction) => (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res, next).catch(next)
+  }
